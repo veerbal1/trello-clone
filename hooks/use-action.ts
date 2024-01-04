c7e938a6caadf6b5ddb0a59c065d79576061b628
Veerbal Singh <Veerbalsingh1@gmail.com>
2024-01-04 20:41:35 +0530

    Fixed: useAction hook to set field errors unconditionally

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -31,10 +31,7 @@ export const useAction = <TInput, TOutput>(
         if (!result) {
           return;
         }
-        if (result.fieldErrors) {
-          setFieldErrors(result.fieldErrors);
-        }
-
+        setFieldErrors(result.fieldErrors);
         if (result.error) {
           setError(result.error);
           options.onError?.(result.error);
