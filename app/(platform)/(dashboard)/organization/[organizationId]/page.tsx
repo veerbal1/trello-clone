11147f0f91f3c100f640b9180f9732327544ae57
Veerbal Singh <Veerbalsingh1@gmail.com>
2024-01-05 10:59:26 +0530

    Created board popover form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,28 +1,14 @@
-import { create } from '@/actions/create-board';
-import { Button } from '@/components/ui/button';
-import { db } from '@/lib/db';
+import { Separator } from '@/components/ui/separator';
+import { Info } from './_components/info';
+import { BoardList } from './_components/board-list';
 
 async function OrganizationIdPage() {
-  const boards = await db.board.findMany();
   return (
-    <div className="flex flex-col space-y-4">
-      <form action={create} className='flex gap-x-4'>
-        <input
-          className="border border-black p-2 rounded"
-          id="title"
-          type="text"
-          placeholder="Title"
-          name="title"
-          required
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <div key={board.id} className="border border-black p-2 rounded">
-            <a href={`/board/${board.id}`}>{board.title}</a>
-          </div>
-        ))}
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
       </div>
     </div>
   );
--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,9 +1,13 @@
 import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from 'sonner';
 
 function PlatformLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='h-full w-full'>
-      <ClerkProvider>{children}</ClerkProvider>
+    <div className="h-full w-full">
+      <ClerkProvider>
+        <Toaster />
+        {children}
+      </ClerkProvider>
     </div>
   );
 }
